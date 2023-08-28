1e5967e47fa473b310c7bb4662e8069496a4f76e
Shashikirana <126816648+Shashikiran1297@users.noreply.github.com>
2023-08-28 17:33:29 +0530

    Adding Food App details

--- first-app/src/Components/WorkCard.jsx
+++ first-app/src/Components/WorkCard.jsx
@@ -28,9 +28,9 @@ const WorkCard = () => {
             <img src={pro1} alt="not found" />
             <h2 className="project-title">Movie Rating App</h2>
             <div className="pro-details">
-                <p>Before Booking our ticket we know the rating of the movie after that we decide book or not.</p>
+                <p>Responsive Hotel Food Order Page </p>
                 <div className="pro-btns">
-                    <NavLink to="https://shashikiran1297.github.io/crazyOtt/" className="btn">
+                    <NavLink to="https://resturent-app-plum.vercel.app/" className="btn">
                       View
                     </NavLink>
                     <NavLink to="url.com" className="btn">
